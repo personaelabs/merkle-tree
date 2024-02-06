97ed7a30d00134604e078c864f33f704b2461de7
Daniel Tehrani <contact@dantehrani.com>
2024-02-06 20:54:42 +0900

    build for nodejs

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,12 +2,8 @@
 new_version=$1
 
 # Build the wasm package
-wasm-pack build && 
+wasm-pack build --scope personaelabs --target nodejs && 
 cd pkg && 
-
-# Rename the name field in package.json to @personaelabs/merkle-tree
-jq '.name="@personaelabs/merkle-tree"' package.json > package.json.tmp && 
-mv package.json.tmp package.json && 
 
 # Set the new version
 npm version $new_version && 
